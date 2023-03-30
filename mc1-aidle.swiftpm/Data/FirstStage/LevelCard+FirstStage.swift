913149b435544c8248a36e39138322430b44c17a
Taehyun <taehyunjo97@gmail.com>
2023-03-30 23:49:30 +0900

    chore: add stage 1 scenario

---
+++ mc1-aidle.swiftpm/Data/FirstStage/LevelCard+FirstStage.swift
@@ -0,0 +1,18 @@
+//
+//  LevelCard+FirstStage.swift
+//  mc1-aidle
+//
+//  Created by byo on 2023/03/30.
+//
+
+import Foundation
+
+extension LevelCard {
+    enum FirstStage {
+        static let juju = LevelCard(
+            firstImageKey: "jujuLv1",
+            secondImageKey: "jujuLv2",
+            nextScene: nil
+        )
+    }
+}
---
+++ mc1-aidle.swiftpm/Data/FirstStage/StoryScene+FirstStage.swift
@@ -0,0 +1,91 @@
+//
+//  StoryScene+FirstStage.swift
+//  mc1-aidle
+//
+//  Created by byo on 2023/03/30.
+//
+
+import Foundation
+
+extension StoryScene {
+    enum FirstStage {
+        static let jujuHi = GeneralStoryScene(
+            speaker: Learner.juju,
+            script: "안녕하세요?",
+            nextScene: FirstStage.noAnswer
+        )
+        
+        static let noAnswer = GeneralStoryScene(
+            speaker: ExtraSpeaker.teamMembers,
+            script: "...",
+            nextScene: FirstStage.jujuStart
+        )
+        
+        static let jujuStart = GeneralStoryScene(
+            speaker: Learner.juju,
+            script: "시작해볼까요?",
+            nextScene: FirstStage.noAnswerAgain
+        )
+        
+        static let noAnswerAgain = GeneralStoryScene(
+            speaker: Learner.juju,
+            script: "...\n\n(사람들이 아무말도 안한다...)",
+            nextScene: FirstStage.jujuWant
+        )
+        
+        static let jujuWant = SelectionStoryScene(
+            speaker: Learner.juju,
+            script: "기획을 해보고 싶긴 했는데...",
+            options: [
+                .init(
+                    text: "댄스 워크숍을 열어서 친해지자",
+                    nextScene: nil
+                ),
+                .init(
+                    text: "아무도 안나서니 박력있게 팀을 리드한다",
+                    nextScene: FirstStage.jujuWinter
+                ),
+                .init(
+                    text: "나대지 말고 말년병장처럼 조용히 꿀 빨자",
+                    nextScene: nil
+                )
+            ]
+        )
+        
+        static let jujuWinter = GeneralStoryScene(
+            speaker: Learner.juju,
+            script: "이번 발표로 에스파 신곡 한번 추실래요?\n제가 윈터할게요!",
+            nextScene: FirstStage.allYes
+        )
+        
+        static let allYes = GeneralStoryScene(
+            speaker: ExtraSpeaker.teamMembers,
+            script: "ㅇㅇ ~~~ x5",
+            nextScene: FirstStage.jujuConfused
+        )
+        
+        static let jujuConfused = SelectionStoryScene(
+            speaker: Learner.juju,
+            script: "왜 다른 의견을 아무도 안내고 다 그저 좋다고만 하지?\n우리 이대로 괜찮은 걸까..",
+            options: [
+                .init(
+                    text: "집 가즈아",
+                    nextScene: nil
+                ),
+                .init(
+                    text: "이렇게 가다가 진짜 망할것같으니 회식을 추진하자",
+                    nextScene: FirstStage.ending
+                ),
+                .init(
+                    text: "데이지 섭 미안해요 MC2부터 잘할게요",
+                    nextScene: nil
+                )
+            ]
+        )
+        
+        static let ending = EndingStoryScene(
+            script: "",
+            nextScene: LevelCard.FirstStage.juju
+        )
+    }
+}
